7b8ffb26777581dc1fd5c698cdeb4f827498e62d
BuSHmaker <BuSHmaker@mail.ru>
2020-06-14 03:39:39 +0300

    Revert "Updated for client 1.9.1.0"
    This reverts commit d21dda9476e5f1b8eaab8d4f158b054b3a148473.

--- source/res/scripts/client/gui/mods/.submodules/source/plugins/AdvancedArtyExtension.py
+++ source/res/scripts/client/gui/mods/.submodules/source/plugins/AdvancedArtyExtension.py
@@ -82,7 +82,7 @@ def new_ArtyCamera_init(self, *args, **kwargs):
 	config = g_config['plugins']['advancedArty']
 	if config['enabled']:
 		if config['cameraAdjustment']['enabled']:
-			self._cfg['interpolationSpeed'] = config['cameraAdjustment']['interpolationSpeed']
+			self._ArtyCamera__cfg['interpolationSpeed'] = config['cameraAdjustment']['interpolationSpeed']
 		if config['orthogonalView']['enabled']:
 			self._ArtyCamera__strategicAreaViewScaleMatrix = XModLib.MathUtils.getIdentityMatrix()
 	return
